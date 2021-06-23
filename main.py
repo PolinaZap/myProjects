3a9e8060f0eb34755a699f2df852675aad50230e
polina <zatula.polina@gmail.com>
2021-06-23 12:53:58 +0300

    calculator

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
-print('Addition(+) = 1. Subtraction(-) = 2. Multiplication(*) = 3. Division(/) = 4. Exponentiation(**) = 5.')
-z = int(input('Print the number of action you want to take:'))
-if z > 5:
+print('Addition = +. Subtraction = -. Multiplication = *. Division = /. Exponentiation = **.')
+z = str(input('Print the number of action you want to take:'))
+if z != '+' and  z != '-' and z != '*' and z != '/' and z != '**':
     print('Please, choose the action!')
-if z < 1:
-    print('Please, choose the action!')
-if z == 1:
+if z == '+':
     a = int(input('Print the first addend:'))
     b = int(input('Print the second addend:'))
     print('The sum is', a + b, '.')
-if z == 2:
+if z == '-':
     a = int(input('Print the minuend:'))
     b = int(input('Print the subtrahend:'))
     print('The difference is', a - b, '.')
-if z == 3:
+if z == '*':
     a = int(input('Print the first multiplier:'))
     b = int(input('Print the second multiplier:'))
     print('The product is', a * b, '.')
-if z == 4:
+if z == '/':
     a = int(input('Print the dividend:'))
     b = int(input('Print the divisor:'))
     if b > 0:
@@ -28,7 +26,7 @@ if z == 4:
             print('You wrote zero again. Division by zero is not possible!')
         else:
             print('The product is', a / r, '.')
-if z == 5:
+if z == '**':
     a = int(input('Print the first number:'))
     b = int(input('Print the second number:'))
     print('The answer is', a ** b, '.')
